1eeef11139aef61d4eed2b251946c9817d76035b
MiccWan <michaelwan0906@hotmail.com>
2020-08-13 10:53:42 +0800

    add wildcard request console

--- src/server/www/index.js
+++ src/server/www/index.js
@@ -7,12 +7,18 @@ export function initExpress() {
   const app = express();
   const server = http.Server(app);
 
-  app.use('/', express.static(path.join(__dirname, 'public')));
+  app.use(/.*/, (req, res, next) => {
+    console.log('Req from:', req.hostname + req.path, (new Date()).toLocaleTimeString());
+    next();
+  });
 
   app.get('/', function (req, res) {
     console.log('GET /');
+    // console.log('returning', path.join(__dirname, 'public', 'index.html'));
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
+
+  app.use(express.static(path.join(__dirname, 'public')));
 
   server.listen(3000, function () {
     console.log('Example app listening on port 3000!');
